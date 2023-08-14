28b245fbb6c9bf96113598e142ebf6af77d9c1bf
Iman Maris <marisiman99@gmail.com>
2023-08-14 11:25:07 +0700

    add modified app

--- dist/app.js
+++ dist/app.js
@@ -13,4 +13,5 @@ app.get("/", (req, res) => {
 });
 app.listen(port, () => {
     console.log(`server listen ${port}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
